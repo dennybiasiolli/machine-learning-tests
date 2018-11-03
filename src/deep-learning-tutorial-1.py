5d57d767504705c2b40aec81f246c043eff41647
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-11-03 18:28:15 +0100

    src: adding L Layer Neural Network

--- src/deep-learning-tutorial-1.py
+++ src/deep-learning-tutorial-1.py
@@ -1,3 +1,7 @@
+from keras.layers import Dense  # build our layers library
+from keras.models import Sequential  # initialize neural network library
+from sklearn.model_selection import cross_val_score
+from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import sys
@@ -296,6 +300,11 @@ print("train accuracy: {} ".format(logreg.fit(
     x_train.T, y_train.T.ravel()).score(x_train.T, y_train.T)))
 
 
+'''
+2-Layer Neural Network
+'''
+
+
 def initialize_parameters_and_layer_sizes_NN(x_train, y_train):
     # intialize parameters and layer sizes
     parameters = {"weight1": np.random.randn(3, x_train.shape[0]) * 0.1,
@@ -411,3 +420,33 @@ def two_layer_neural_network(x_train, y_train, x_test, y_test, num_iterations):
 
 parameters = two_layer_neural_network(
     x_train, y_train, x_test, y_test, num_iterations=2500)
+
+
+'''
+L Layer Neural Network
+'''
+# reshaping
+x_train, x_test, y_train, y_test = x_train.T, x_test.T, y_train.T, y_test.T
+
+# Evaluating the ANN
+
+
+def build_classifier():
+    classifier = Sequential()  # initialize neural network
+    classifier.add(Dense(units=8, kernel_initializer='uniform',
+                         activation='relu', input_dim=x_train.shape[1]))
+    classifier.add(
+        Dense(units=4, kernel_initializer='uniform', activation='relu'))
+    classifier.add(
+        Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn=build_classifier, epochs=100)
+accuracies = cross_val_score(estimator=classifier, X=x_train, y=y_train, cv=3)
+mean = accuracies.mean()
+variance = accuracies.std()
+print("Accuracy mean: " + str(mean))
+print("Accuracy variance: " + str(variance))
