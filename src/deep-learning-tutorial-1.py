5cdddcbc3d7401a696c46c51515beeee6b1bc918
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-11-03 18:10:42 +0100

    src: creating 2-layer neural network

--- src/deep-learning-tutorial-1.py
+++ src/deep-learning-tutorial-1.py
@@ -294,3 +294,120 @@ print("test accuracy: {} ".format(logreg.fit(
     x_train.T, y_train.T.ravel()).score(x_test.T, y_test.T)))
 print("train accuracy: {} ".format(logreg.fit(
     x_train.T, y_train.T.ravel()).score(x_train.T, y_train.T)))
+
+
+def initialize_parameters_and_layer_sizes_NN(x_train, y_train):
+    # intialize parameters and layer sizes
+    parameters = {"weight1": np.random.randn(3, x_train.shape[0]) * 0.1,
+                  "bias1": np.zeros((3, 1)),
+                  "weight2": np.random.randn(y_train.shape[0], 3) * 0.1,
+                  "bias2": np.zeros((y_train.shape[0], 1))}
+    return parameters
+
+
+def forward_propagation_NN(x_train, parameters):
+
+    Z1 = np.dot(parameters["weight1"], x_train) + parameters["bias1"]
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(parameters["weight2"], A1) + parameters["bias2"]
+    A2 = sigmoid(Z2)
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+
+    return A2, cache
+
+
+def compute_cost_NN(A2, Y, parameters):
+    # Compute cost
+    logprobs = np.multiply(np.log(A2), Y)
+    cost = -np.sum(logprobs)/Y.shape[1]
+    return cost
+
+
+def backward_propagation_NN(parameters, cache, X, Y):
+    # Backward Propagation
+    dZ2 = cache["A2"]-Y
+    dW2 = np.dot(dZ2, cache["A1"].T)/X.shape[1]
+    db2 = np.sum(dZ2, axis=1, keepdims=True)/X.shape[1]
+    dZ1 = np.dot(parameters["weight2"].T, dZ2)*(1 - np.power(cache["A1"], 2))
+    dW1 = np.dot(dZ1, X.T)/X.shape[1]
+    db1 = np.sum(dZ1, axis=1, keepdims=True)/X.shape[1]
+    grads = {"dweight1": dW1,
+             "dbias1": db1,
+             "dweight2": dW2,
+             "dbias2": db2}
+    return grads
+
+
+def update_parameters_NN(parameters, grads, learning_rate=0.01):
+    # update parameters
+    parameters = {
+        "weight1": parameters["weight1"]-learning_rate*grads["dweight1"],
+        "bias1": parameters["bias1"]-learning_rate*grads["dbias1"],
+        "weight2": parameters["weight2"]-learning_rate*grads["dweight2"],
+        "bias2": parameters["bias2"]-learning_rate*grads["dbias2"]
+    }
+    return parameters
+
+
+def predict_NN(parameters, x_test):
+    # prediction
+
+    # x_test is a input for forward propagation
+    A2, cache = forward_propagation_NN(x_test, parameters)
+    Y_prediction = np.zeros((1, x_test.shape[1]))
+    # if z is bigger than 0.5, our prediction is sign one (y_head=1),
+    # if z is smaller than 0.5, our prediction is sign zero (y_head=0),
+    for i in range(A2.shape[1]):
+        if A2[0, i] <= 0.5:
+            Y_prediction[0, i] = 0
+        else:
+            Y_prediction[0, i] = 1
+
+    return Y_prediction
+
+
+def two_layer_neural_network(x_train, y_train, x_test, y_test, num_iterations):
+    # 2 - Layer neural network
+    cost_list = []
+    index_list = []
+    # initialize parameters and layer sizes
+    parameters = initialize_parameters_and_layer_sizes_NN(x_train, y_train)
+
+    for i in range(0, num_iterations):
+         # forward propagation
+        A2, cache = forward_propagation_NN(x_train, parameters)
+        # compute cost
+        cost = compute_cost_NN(A2, y_train, parameters)
+        # backward propagation
+        grads = backward_propagation_NN(parameters, cache, x_train, y_train)
+        # update parameters
+        parameters = update_parameters_NN(parameters, grads)
+
+        if i % 100 == 0:
+            cost_list.append(cost)
+            index_list.append(i)
+            print("Cost after iteration %i: %f" % (i, cost))
+    plt.plot(index_list, cost_list)
+    plt.xticks(index_list, rotation='vertical')
+    plt.xlabel("Number of Iterarion")
+    plt.ylabel("Cost")
+    plt.show()
+
+    # predict
+    y_prediction_test = predict_NN(parameters, x_test)
+    y_prediction_train = predict_NN(parameters, x_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(
+        100 - np.mean(np.abs(y_prediction_train - y_train)) * 100))
+    print("test accuracy: {} %".format(
+        100 - np.mean(np.abs(y_prediction_test - y_test)) * 100))
+    return parameters
+
+
+parameters = two_layer_neural_network(
+    x_train, y_train, x_test, y_test, num_iterations=2500)
